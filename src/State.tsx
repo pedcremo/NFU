483fbff202c725f084bba20ea66e7d8813d7bc2a
nicklaus_ <pedcremo@gmail.com>
2021-01-19 21:45:51 +0100

    State logger

--- src/State.tsx
+++ src/State.tsx
@@ -25,12 +25,24 @@ let reducer = (state, action) => {
   return state;
 };
 
+const logger = (reducer) => {
+  const reducerWithLogger = (state, action) => {
+    console.log("%cPrevious State:", "color: #9E9E9E; font-weight: 700;", state);
+    console.log("%cAction:", "color: #00A7F7; font-weight: 700;", action);
+    console.log("%cNext State:", "color: #47B04B; font-weight: 700;", reducer(state,action));
+    return reducer(state,action);
+  };
+  return reducerWithLogger;
+}
+
+const loggerReducer = logger(reducer);
+
 function AppContextProvider(props) {
   const fullInitialState = {
     ...initialState,
   }
 
-  let [state, dispatch] = useReducer(reducer, fullInitialState);
+  let [state, dispatch] = useReducer(loggerReducer, fullInitialState);
   let value = { state, dispatch };
 
 
