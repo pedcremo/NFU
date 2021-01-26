f571b7187a540c18dd9067deeee5bffe867ff310
j-maestre <xemaiestacio@gmail.com>
2021-01-26 20:05:12 +0100

    prueba git ignore

--- src/components/maps/mapContainer.js
+++ src/components/maps/mapContainer.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import credentials from '../../credentials.json';
  
 export class MapContainer extends React.Component {
   render() {
     console.log("PROPS GOOGLE")
     console.log(this.props.google)
-
+    console.log("CREDENTIALS")
+    console.log(credentials)
     return (
       <Map 
       google={this.props.google} 
       
-      zoom={15}
+      zoom={2}
       onClick={this.mapClicked}
       >
 
@@ -34,7 +36,7 @@ export class MapContainer extends React.Component {
 }
  
 export default GoogleApiWrapper({
-  key: "AIzaSyBqLziFyS_qTYHkdVjUwo2AMGhM6tmViqQ"  //La primera api que he hecho
+  key: credentials.key  //La primera api que he hecho
   // apiKey: "AIzaSyBr8oaQDhieux3kxWJeoeHblUbAVv2A5u8"  //La segunda api que he hecho
   //https://console.cloud.google.com/apis/credentials?authuser=3&_ga=2.5136755.1506244662.1611601178-476118781.1611601178&angularJsUrl=%2Fprojectselector%2Fapis%2Fcredentials%3Fauthuser%3D3%26_ga%3D2.5136755.1506244662.1611601178-476118781.1611601178%26supportedpurview%3Dproject%26folder%3Dtrue%26organizationId%3Dtrue&project=nosfaltauno&folder=&organizationId=&supportedpurview=project
 })(MapContainer)
