8ff206a3c27752ca840973ad227d8c4b777c300f
vicnx <andanivicente@gmail.com>
2021-02-09 21:36:34 +0100

    new render test password_forgotten

--- src/pages/recover_password/password_forgotten.test.tsx
+++ src/pages/recover_password/password_forgotten.test.tsx
@@ -1,10 +1,23 @@
 import React from "react";
 import renderer from "react-test-renderer";
 import PasswordForgotten from "./password_forgotten";
+import App from "../../App";
+import { render, screen, fireEvent } from '@testing-library/react';
 
 it("renders correctly", () => {
-  const tree = renderer
-    .create(<PasswordForgotten></PasswordForgotten>)
-    .toJSON();
-  expect(tree).toMatchSnapshot();
+  const baseElement = render(<PasswordForgotten />);
+  
+  expect(baseElement).toBeDefined();
+  //We check if there is any item with EMail placeholder (the input)
+  expect(screen.getByPlaceholderText('Email')).toBeInTheDocument()
+  expect(screen.getByRole('button')).toBeInTheDocument()
 });
+
+//Test send button with valid email
+// it('test email valid',() =>{
+//   const email = "test@test.com";
+//   const baseElement = render(<PasswordForgotten />);
+//   fireEvent.change(screen.getByPlaceholderText('Email'), {
+//     target: { value: email },
+//   });
+// })
