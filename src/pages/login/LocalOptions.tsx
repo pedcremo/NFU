1c3756b01c940400fbec129230a8d3a4c110da6a
Jooui <jrevertvila@gmail.com>
2021-02-10 00:46:29 +0100

    Refactoring Login Finished

--- src/pages/login/LocalOptions.tsx
+++ src/pages/login/LocalOptions.tsx
@@ -1,12 +1,10 @@
 import React, { useContext, useState, useRef } from "react";
 import { AppContext } from "../../State";
 import { useHistory } from "react-router-dom";
-
-import { IonLabel, IonInput, IonLoading } from '@ionic/react';
+import { IonLabel, IonInput, IonLoading, IonButton } from '@ionic/react';
 import './LocalOptions.css';
 
-
-const LocalOptions: React.FC<{action: Function}> = ({action}) => {
+const LocalOptions: React.FC<{ action: Function }> = ({ action }) => {
     const history = useHistory()
     const { dispatch } = useContext(AppContext);
     const [email, setEmail] = useState<React.ReactText | undefined>("");
@@ -18,33 +16,26 @@ const LocalOptions: React.FC<{action: Function}> = ({action}) => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-          setShowLoading(true);
-          setTimeout(() => dispatch({ type: "SET_USER", value: email }), 5000);
+            setShowLoading(true);
+            setTimeout(() => dispatch({ type: "SET_USER", value: email }), 5000);
         } catch (e) {
-          console.error(e);
-          setShowLoading(false);
-          setFormErrors(e);
+            console.error(e);
+            setShowLoading(false);
+            setFormErrors(e);
         }
     };
-    
+
     return (
         <form onSubmit={handleSubmit} method="post" name="login_form" ref={formRef} className="loginOptionsContainer_form">
             <IonLoading isOpen={showLoading} message={"Logging in"} onDidDismiss={() => setShowLoading(false)} />
-            <div className="loginInputsContainer" id="localOps">
-                <div className="loginOption loginOption--input">
-                    <IonInput type="email" required value={email} className="inputFieldLogin" onInput={(e) => setEmail(e.currentTarget.value)} placeholder="Email.." />
-                </div>
-                <div className="loginOption loginOption--input">
-                    <IonInput type="password" value={password} className="inputFieldLogin" required onInput={(e) => setPassword(e.currentTarget.value)} placeholder="Password.." />
-                </div>
-                <button className="loginOption loginOption--submit" type="submit" id="btnLogin" > Log In </button>
-                <div className="login-row">
-                    <IonLabel className="loginOption loginOption--local loginOption--create" onClick={() => history.push("recover")} >I've forgot my password</IonLabel>
-                    <div className="separator"></div>
-                    <IonLabel className="loginOption loginOption--local loginOption--create" id="createBtnLink" onClick={() => history.push("register")} >Create an account</IonLabel>
-                </div>
-                <IonLabel className="loginOption loginOption--local" onClick={() => action('Social')} > Continue with Google or Facebook</IonLabel>
+            <IonInput type="email" required value={email} className="loginOption loginOption--input inputFieldLogin" onInput={(e) => setEmail(e.currentTarget.value)} placeholder="Email.." />
+            <IonInput type="password" value={password} className="loginOption loginOption--input inputFieldLogin" required onInput={(e) => setPassword(e.currentTarget.value)} placeholder="Password.." />
+            <IonButton className="loginOption loginOption--submit" type="submit" id="btnLogin" > Log In </IonButton>
+            <div className="login-row">
+                <IonLabel className="loginOption loginOption--local loginOption--create" onClick={() => history.push("recover")} >I've forgot my password</IonLabel>
+                <IonLabel className="loginOption loginOption--local loginOption--create" id="createBtnLink" onClick={() => history.push("register")} >Create an account</IonLabel>
             </div>
+            <IonLabel className="loginOption loginOption--local" onClick={() => action('Social')} > Continue with Google or Facebook</IonLabel>
         </form>
     )
 }
