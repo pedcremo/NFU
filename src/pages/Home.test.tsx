79c2d3fac8db35289fe5b6ff7708480d61d9d5dd
tonomolla6 <tonomollag6@gmail.com>
2021-02-14 16:28:55 +0100

    Tono -> Pruebas

---
+++ src/pages/Home.test.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { fireEvent, render, screen } from "@testing-library/react";
+import Home from "./Home";
+import { AppContextProvider } from "../State";
+import { Route, Router, BrowserRouter } from "react-router-dom";
+
+const renderWithRouter = (ui, { route = "/" } = {}) => {
+  window.history.pushState({}, "Test page", route);
+
+  return render(ui, { wrapper: BrowserRouter });
+};
+
+// Comprobamos si el home se pinta bien
+it("Render HOME corrently", () => {
+  const { baseElement, container } = renderWithRouter(
+    <AppContextProvider>
+      <Home />
+    </AppContextProvider>
+  );
+  expect(baseElement).toBeDefined();
+
+  // Comprobamos si los componentes se pintan correctamente.
+  const modal_maps = container.querySelector("ion-modal");
+  expect(modal_maps).toBeDefined();
+});
