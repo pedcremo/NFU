03bd6098268782d1e243f85b308b12220302c0ae
vicnx <andanivicente@gmail.com>
2021-02-16 13:31:01 +0100

    translated header notifications

--- src/pages/notifications/Notifications.tsx
+++ src/pages/notifications/Notifications.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Notifications.css";
-import events from '../../data/data.json';
+import events from "../../data/data.json";
+import { useTranslation } from "react-i18next";
 
 import {
   IonContent,
@@ -21,9 +22,10 @@ import { checkmark, close } from "ionicons/icons";
 import EventsPreview from "../../components/Event/EventsPreview";
 
 const Notifications: React.FC = () => {
+  const { t } = useTranslation();
   return (
     <IonPage>
-      <Header page={"Notifications"} />
+      <Header page={t("pages.notifications")} />
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
@@ -32,20 +34,34 @@ const Notifications: React.FC = () => {
         </IonHeader>
         <IonList className="eventsList">
           {Object.values(events.events).map((event, index) => (
-            <IonItemSliding key={"item" + index} className="fit  ion-no-padding  primary--bg">
+            <IonItemSliding
+              key={"item" + index}
+              className="fit  ion-no-padding  primary--bg"
+            >
               <IonItem className="ion-no-padding  primary--bg">
                 <EventsPreview key={"event_" + index} event={event} />
               </IonItem>
-              <IonItemOptions className="ion-no-padding  primary--bg" side="end">
-                  <IonItemOption color="primary" onClick={() => alert("Accept")}>
-                    <IonIcon style={{fontSize: '2rem'}} color="light" icon={checkmark}/>
-                  </IonItemOption>
-                </IonItemOptions>
-                <IonItemOptions className="danger--bg" side="start">
-                    <IonItemOption color="danger" onClick={() => alert("Deny")}>
-                        <IonIcon style={{fontSize: '2rem'}} color="light" icon={close}/>
-                    </IonItemOption>
-                </IonItemOptions>
+              <IonItemOptions
+                className="ion-no-padding  primary--bg"
+                side="end"
+              >
+                <IonItemOption color="primary" onClick={() => alert("Accept")}>
+                  <IonIcon
+                    style={{ fontSize: "2rem" }}
+                    color="light"
+                    icon={checkmark}
+                  />
+                </IonItemOption>
+              </IonItemOptions>
+              <IonItemOptions className="danger--bg" side="start">
+                <IonItemOption color="danger" onClick={() => alert("Deny")}>
+                  <IonIcon
+                    style={{ fontSize: "2rem" }}
+                    color="light"
+                    icon={close}
+                  />
+                </IonItemOption>
+              </IonItemOptions>
             </IonItemSliding>
           ))}
         </IonList>
