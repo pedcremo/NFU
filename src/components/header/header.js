ffb90f3dad3a7010884975d267f41c62c836ea46
vicnx <andanivicente@gmail.com>
2021-02-16 12:57:27 +0100

    En el header se muestra el usuario logeado (si no hay, se muestra el boton de login)

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,91 +1,60 @@
-import React, { useContext, useState, useEffect } from 'react';
-import { AppContext } from '../../State';
-import { Redirect } from "react-router-dom";
-import { ellipsisVertical } from 'ionicons/icons';
+import React, { useContext, useState, useEffect } from "react";
+import { AppContext } from "../../State";
+import { Redirect, Link } from "react-router-dom";
+import { ellipsisVertical } from "ionicons/icons";
 
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonButtons,
+  IonLabel,
+  IonMenuButton,
+  IonChip,
+  IonAvatar,
+} from "@ionic/react";
+import "./header.css";
 
-import {  
-    IonHeader,
-    IonToolbar,
-    IonTitle,
-    IonButtons,
-    IonButton,
-    IonIcon,
-    IonPopover,
-    IonContent,
-    IonList,
-    IonLabel,
-    IonItem,
-    IonMenuButton
-} from '@ionic/react';
-import './header.css'
-
-const Header = (props) =>{
-  const { state,dispatch } = useContext(AppContext);
-  const [showUserMenuEvent, setShowUserMenuEvent] = useState(null);
+const Header = (props) => {
+  const { state, dispatch } = useContext(AppContext);
 
   useEffect(() => {
-    state.theme === "Dark" ? document.body.classList.add("dark") : document.body.classList.remove("dark")
-  })
+    state.theme === "Dark"
+      ? document.body.classList.add("dark")
+      : document.body.classList.remove("dark");
+  });
 
-  if (state.welcome !== 'true'){return <Redirect to="/welcome" />}
-  
-  const doLogout = async () => {  
-    setShowUserMenuEvent(null);          
-    dispatch({type:'LOGOUT'});
-    window.location.reload();  
-  };
-  
+  if (state.welcome !== "true") {
+    return <Redirect to="/welcome" />;
+  }
+
   const page = props.page;
-    return (
-      <>
-        <IonHeader>
-          <IonToolbar>
-            <IonButtons slot="start">
-              <IonMenuButton></IonMenuButton>
-            </IonButtons>
-            <IonTitle>{page}</IonTitle>
-            <IonButtons slot="end">
-              <IonButton
-                fill="clear"
-                onClick={(e) => {
-                  e.persist();
-                  setShowUserMenuEvent(e);
-                }}
-              >
-                <IonIcon icon={ellipsisVertical} />
-              </IonButton>
-            </IonButtons>
-          </IonToolbar>
-        </IonHeader>
-        <IonPopover
-          event={showUserMenuEvent}
-          isOpen={!!showUserMenuEvent}
-          onDidDismiss={() => setShowUserMenuEvent(null)}
-        >
-          <IonContent>
-            <IonList>
-              <IonItem
-                onClick={(e) => {
-                  e.preventDefault();
-                  doLogout();
-                }}
-                detail={true}
-                href=""
-              >
-                <IonLabel>LOGOUT</IonLabel>
-              </IonItem>
-              <IonItem routerLink={'/settings'} detail={true}>
-                <IonLabel>Settings</IonLabel>
-              </IonItem>
-              <IonItem>
+  return (
+    <>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonMenuButton></IonMenuButton>
+          </IonButtons>
+          <IonTitle>{page}</IonTitle>
+          {state.user ? (
+            <Link to="/profile" slot="end" className="navbar-user-link">
+              <IonChip className="navbar-user">
+                <IonAvatar>
+                  <img src="https://gravatar.com/avatar/dba6bae8c566f9d4041fb9cd9ada7741?d=identicon&f=y" />
+                </IonAvatar>
                 <IonLabel>{state.user}</IonLabel>
-              </IonItem>
-            </IonList>
-          </IonContent>
-        </IonPopover>
-      </>
-    );
-}
+              </IonChip>
+            </Link>
+          ) : (
+            <Link to="/login" slot="end" className="navbar-login">
+              <IonLabel>Login</IonLabel>
+            </Link>
+          )}
+        </IonToolbar>
+      </IonHeader>
+    </>
+  );
+};
 
 export default Header;
