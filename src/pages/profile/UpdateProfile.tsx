f015be5d817ed6afa0fae57311ed281f813eb478
vicnx <andanivicente@gmail.com>
2021-02-17 13:27:03 +0100

    Update Profile working Now you can modify your profile with name, surname etc..
    That adds those fields to the state user.

--- src/pages/profile/UpdateProfile.tsx
+++ src/pages/profile/UpdateProfile.tsx
@@ -1,44 +1,44 @@
-import React, { useContext, useState } from 'react';
-import { AppContext } from '../../State';
-import { useHistory } from 'react-router-dom';
-import { useTranslation } from 'react-i18next';
+import React, { useContext, useState } from "react";
+import { AppContext } from "../../State";
+import { useHistory } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 import {
   IonContent,
-  IonHeader,
   IonPage,
-  IonTitle,
-  IonToolbar,
   IonButton,
   IonLabel,
   IonInput,
   IonItem,
-  IonList,
   IonAvatar,
   IonSelect,
   IonSelectOption,
   IonDatetime,
-  IonButtons,
-  IonMenuButton
-} from '@ionic/react';
-import './UpdateProfile.css';
-
+  IonLoading,
+} from "@ionic/react";
+import "./UpdateProfile.css";
+import Header from "../../components/header/header";
 
 const UpdateProfile = () => {
-
   const { state, dispatch } = useContext(AppContext);
   const { t } = useTranslation();
-
-  const [name, setName] = useState<React.ReactText | undefined>('');
-  const [username, setUsername] = useState<React.ReactText | undefined>('');
-  const [website, setWebsite] = useState<React.ReactText | undefined>('');
-  const [bio, setBio] = useState<React.ReactText | undefined>('');
-  const [image, seImage] = useState<React.ReactText | undefined>('');
-
-  const [, setEmail] = useState<React.ReactText | undefined>('');
-  const [number, setNumber] = useState<React.ReactText | undefined>('');
-  const [gender, setGender] = useState<React.ReactText | undefined>('');
-  const [birthday, setBirthday] = useState<string>('');
+  const [name, setName] = useState<React.ReactText | undefined>(
+    state.user.name
+  );
+  const [username, setUsername] = useState<React.ReactText | undefined>(
+    state.user.username
+  );
+  const [gender, setGender] = useState<React.ReactText | undefined>(
+    state.user.gender
+  );
+  const [email, setEmail] = useState<React.ReactText | undefined>(
+    state.user.email
+  );
+  const [surname, setSurname] = useState<React.ReactText | undefined>(
+    state.user.surname
+  );
+  const [birthday, setBirthday] = useState<string>(state.user.birthday);
   const history = useHistory();
+  const [showLoading, setShowLoading] = useState(false);
 
   // Convert selected image to base64 and dispatch the new user's state
   function encodeImageFileAsURL(el) {
@@ -46,13 +46,34 @@ const UpdateProfile = () => {
     if (file) {
       var reader = new FileReader();
       reader.onloadend = function () {
-        let user = state.user
+        let user = state.user;
         user.image = reader.result;
-        dispatch({ type: "SET_USER", value: user })
-      }
+        dispatch({ type: "SET_USER", value: user });
+      };
       reader.readAsDataURL(file);
     }
   }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setShowLoading(true);
+      let user = state.user;
+      user.name = name;
+      user.gender = gender;
+      user.surname = surname;
+      user.birthday = birthday;
+      user.username = username;
+
+      //Here send email with unique token.
+      setTimeout(() => {
+        dispatch({ type: "SET_USER", value: user });
+        setShowLoading(false);
+      }, 1000);
+    } catch (e) {
+      setShowLoading(false);
+    }
+  };
 
   if (!state.user) {
     history.push("/");
@@ -60,21 +81,13 @@ const UpdateProfile = () => {
 
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonButtons slot="start">
-            <IonMenuButton></IonMenuButton>
-          </IonButtons>
-          <IonTitle>{t('updateProfile.title')}</IonTitle>
-        </IonToolbar>
-      </IonHeader>
+      <Header page={t("updateProfile.title")} />
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">{t('updateProfile.title')}</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-
+        <IonLoading
+          isOpen={showLoading}
+          message={"Updating..."}
+          onDidDismiss={() => setShowLoading(false)}
+        />
         <section className="update_profile">
           <div className="profile_photo">
             <IonAvatar className="profile_avatar">
@@ -85,97 +98,85 @@ const UpdateProfile = () => {
               />
             </IonAvatar>
             <IonLabel className="change_profile_photo">
-              {t('updateProfile.changePhoto')}
+              {t("updateProfile.changePhoto")}
             </IonLabel>
             {/* <IonInput type="file" accept=".jpg,.jpeg,.png" multiple="false"> */}
-            <input type="file" id="uploadImgProfile" onChange={(el) => encodeImageFileAsURL(el)} />
+            <input
+              type="file"
+              id="uploadImgProfile"
+              onChange={(el) => encodeImageFileAsURL(el)}
+            />
           </div>
 
-          <IonList>
+          <form className="form_update_profile" onSubmit={handleSubmit}>
             <IonItem>
-              <IonLabel position={"fixed"}>{t('updateProfile.form.name')}</IonLabel>
+              <IonLabel position="floating">
+                {t("updateProfile.form.username")}
+              </IonLabel>
               <IonInput
-                type="text"
-                required
-                value={name}
-                onInput={(e) => setName(e.currentTarget.value)}
-              />
-            </IonItem>
-            <IonItem>
-              <IonLabel position={"fixed"}>{t('updateProfile.form.username')}</IonLabel>
-              <IonInput
-                type="text"
-                required
                 value={username}
                 onInput={(e) => setUsername(e.currentTarget.value)}
               />
             </IonItem>
             <IonItem>
-              <IonLabel position={"fixed"}>{t('updateProfile.form.website')}</IonLabel>
-              <IonInput
-                type="text"
-                value={website}
-                onInput={(e) => setWebsite(e.currentTarget.value)}
-              />
+              <IonLabel position="floating">
+                {t("updateProfile.form.email")}
+              </IonLabel>
+              <IonInput type="email" value={email} disabled />
             </IonItem>
-            <IonItem>
-              <IonLabel position={"fixed"}>{t('updateProfile.form.bio')}</IonLabel>
-              <IonInput
-                type="text"
-                required
-                value={bio}
-                onInput={(e) => setBio(e.currentTarget.value)}
-              />
-            </IonItem>
-
-            <div className="personal_information">
-              <div className="personal_info">
-                <h2>{t('updateProfile.personal.title')}</h2>
-                <IonLabel>{t('updateProfile.personal.description')}</IonLabel>
-              </div>
-
-              <IonItem>
-                <IonLabel position={"fixed"}>{t('updateProfile.form.advanced.email')}</IonLabel>
+            <div className="form_section_input">
+              <IonItem className="form_section_input--input">
+                <IonLabel position="floating">
+                  {t("updateProfile.form.name")}
+                </IonLabel>
                 <IonInput
-                  type="text"
-                  required
-                  value={state.user.email}
-                  onInput={(e) => setEmail(e.currentTarget.value)}
+                  value={name}
+                  onInput={(e) => setName(e.currentTarget.value)}
                 />
               </IonItem>
-              <IonItem>
-                <IonLabel position={"fixed"}>{t('updateProfile.form.advanced.phone')}</IonLabel>
+              <IonItem className="form_section_input--input">
+                <IonLabel position="floating">
+                  {t("updateProfile.form.surname")}
+                </IonLabel>
                 <IonInput
-                  type="text"
-                  value={number}
-                  onInput={(e) => setNumber(e.currentTarget.value)}
+                  value={surname}
+                  onInput={(e) => setSurname(e.currentTarget.value)}
                 />
-              </IonItem>
-              <IonItem>
-                <IonLabel>{t('updateProfile.form.advanced.gender.title')}</IonLabel>
-                <IonSelect
-                  value={gender}
-                  placeholder="Select One"
-                  onIonChange={(e) => setGender(e.detail.value)}
-                >
-                  <IonSelectOption value="Prefer not to say">{t('updateProfile.form.advanced.gender.prefer')}</IonSelectOption>
-                  <IonSelectOption value="Female">{t('updateProfile.form.advanced.gender.female')}</IonSelectOption>
-                  <IonSelectOption value="Male">{t('updateProfile.form.advanced.gender.male')}</IonSelectOption>
-                </IonSelect>
-              </IonItem>
-              <IonItem>
-                <IonLabel>{t('updateProfile.form.advanced.birthday')}</IonLabel>
-                <IonDatetime
-                  displayFormat="MM DD YY"
-                  placeholder="Select Date"
-                  value={birthday}
-                  onIonChange={(e) => setBirthday(e.detail.value!)}
-                ></IonDatetime>
               </IonItem>
             </div>
-
-            <IonButton data-testid="update-profile-button" expand="block" type="submit">{t('updateProfile.form.edit')}</IonButton>
-          </IonList>
+            <IonItem>
+              <IonLabel>
+                {t("updateProfile.form.advanced.gender.title")}
+              </IonLabel>
+              <IonSelect
+                value={gender}
+                placeholder="Select One"
+                onIonChange={(e) => setGender(e.detail.value)}
+              >
+                <IonSelectOption value="Prefer not to say">
+                  {t("updateProfile.form.advanced.gender.prefer")}
+                </IonSelectOption>
+                <IonSelectOption value="Female">
+                  {t("updateProfile.form.advanced.gender.female")}
+                </IonSelectOption>
+                <IonSelectOption value="Male">
+                  {t("updateProfile.form.advanced.gender.male")}
+                </IonSelectOption>
+              </IonSelect>
+            </IonItem>
+            <IonItem>
+              <IonLabel>{t("updateProfile.form.advanced.birthday")}</IonLabel>
+              <IonDatetime
+                displayFormat="MM DD YY"
+                placeholder="Select Date"
+                value={birthday}
+                onIonChange={(e) => setBirthday(e.detail.value!)}
+              ></IonDatetime>
+            </IonItem>
+            <IonButton className="ion-margin-top" type="submit" expand="block">
+              {t("updateProfile.form.edit")}
+            </IonButton>
+          </form>
         </section>
       </IonContent>
     </IonPage>
