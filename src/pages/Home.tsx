6cbb5d2cfdda2c06397b0df96405e7647d3f0f54
j-maestre <xemaiestacio@gmail.com>
2021-02-18 12:28:56 +0100

    add test

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,8 @@ import React, { useContext } from "react";
 import { AppContext } from "../State";
 
 import { IonContent, IonPage } from "@ionic/react";
+import { Geolocation } from '@ionic-native/geolocation';
+
 
 import "./Home.css";
 import EventList from "../components/Event/Event_List";
@@ -10,21 +12,25 @@ import Header from "../components/header/header";
 
 import { useTranslation } from "react-i18next";
 
-import { Geolocation } from '@ionic-native/geolocation';
-
 const Home = () => {
   const { dispatch } = useContext(AppContext);
   const { t } = useTranslation();
+  const geolocation = Geolocation;
 
   (function () {
-    Geolocation.getCurrentPosition().then(pos => {
-      // console.log('lat: ' + pos.coords.latitude + ', lon: ' + pos.coords.longitude);
-      let coords = {
-        latitude: pos.coords.latitude,
-        longitude: pos.coords.longitude,
-      }
-      dispatch({ type: "USER_COORDINATES", value: coords });
-    });
+    try{
+      geolocation.getCurrentPosition().then(pos => {
+        // console.log('lat: ' + pos.coords.latitude + ', lon: ' + pos.coords.longitude);
+        let coords = {
+          latitude: pos.coords.latitude,
+          longitude: pos.coords.longitude,
+        }
+        dispatch({ type: "USER_COORDINATES", value: coords });
+      });
+    }catch(e){
+      console.log("Error get location: ", e)
+    }
+    
   })();
 
   return (
