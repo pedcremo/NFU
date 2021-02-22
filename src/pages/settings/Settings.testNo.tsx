abc9c844453903beca1fc2d4c648600d80e23f1f
Jooui <jrevertvila@gmail.com>
2021-02-22 13:06:36 +0100

    Making CreateEvent test and Settings test

---
+++ src/pages/settings/Settings.testNo.tsx
@@ -0,0 +1,48 @@
+import React, { useContext, useState, useEffect } from "react";
+import { AppContextProvider, AppContext } from '../../State';
+import { fireEvent, render, screen } from '@testing-library/react';
+import Settings from "./Settings";
+import { generateGravatar, imageLocal } from "../../utils";
+import { BrowserRouter } from "react-router-dom";
+
+const renderWithRouter = (ui, { route = "/" } = {}) => {
+  window.history.pushState({}, "Test page", route);
+  return render(ui, { wrapper: BrowserRouter });
+};
+
+const TestSettingsInit = () => {
+  const { state, dispatch } = useContext(AppContext);
+  const user = {
+    email: "example@gmail.com",
+    username: "example",
+    image:
+      state.currentAvatar === "gravatar"
+        ? generateGravatar("example@gmail.com")
+        : imageLocal,
+    imageLocal: imageLocal,
+    events_joined: []
+  };
+  useEffect(() => {
+    dispatch({type:'WELCOME',value:'true' as React.ReactText})
+    dispatch({ type: 'SET_USER', value: user })
+  }, [])
+
+  // return(<>{setTimeout(() => {
+  //   return (<Settings/>)
+  // }, 500)}</>)
+  return <Settings/>
+
+}
+
+
+test("renders Component Settings", () => {
+
+  const { baseElement, container } = render(<AppContextProvider><TestSettingsInit /></AppContextProvider>);
+  expect(baseElement).toBeDefined();
+
+  const darkToggle = container.querySelector('#dark-toggle')
+  screen.debug();
+  console.log(container);
+  console.log(darkToggle);
+
+});
