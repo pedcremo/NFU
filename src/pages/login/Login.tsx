e050242042cb82e4f38b64b4efcba037f1836da9
ogubeda <og.ubeda@gmail.com>
2021-02-23 19:54:47 +0100

    EASY 4

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,7 +1,7 @@
-import React, { useContext, useState} from "react";
+import React, { useContext, useState } from "react";
 import { AppContext } from "../../State";
-import { Redirect } from "react-router-dom";
-import { IonContent, IonPage, IonImg, IonLabel } from "@ionic/react";
+import { Redirect, useHistory } from "react-router-dom";
+import { IonContent, IonPage, IonImg, IonLabel, IonItem } from "@ionic/react";
 import "./login.css";
 import DeporteImg from "../../assets/img/deporte_img.png";
 import AppTitle from '../../components/shared/AppTitle'
@@ -11,21 +11,27 @@ import { useTranslation } from "react-i18next";
 
 const Login: React.FC = () => {
   const { state } = useContext(AppContext);
-  const [ currentOptions, setCurrentOptions] = useState<React.ReactText | undefined>('Social');
+  const [currentOptions, setCurrentOptions] = useState<React.ReactText | undefined>('Social');
   const { t } = useTranslation();
+  const history = useHistory();
 
   /* istanbul ignore if */
   if (state.welcome !== 'true') { return <Redirect to="/welcome" /> }
   if (state.user) return <Redirect to="/app/home" />;
-//"initial_text":"Find tournaments and matches for the sport that you prefer",
+
+  //"initial_text":"Find tournaments and matches for the sport that you prefer",
   return (
     <IonPage>
       <IonContent fullscreen>
         <div className="loginPageContent">
           <AppTitle />
           <IonImg src={DeporteImg} alt="Deporte IMG" className="loginImg" />
-          <IonLabel className="prhaseLogin">{t("login.initial_text")}</IonLabel> 
-          { currentOptions === "Social" ? <SocialOptions action={setCurrentOptions}/> : <LocalOptions action={setCurrentOptions}/> }
+          <IonLabel className="prhaseLogin">{t("login.initial_text")}</IonLabel>
+          <br></br>
+          <IonItem className="socialOption socialOption--local" onClick={() => history.push('app/home')}>
+            <IonLabel className="socialOption--local-label">Return</IonLabel>
+          </IonItem>
+          {currentOptions === "Social" ? <SocialOptions action={setCurrentOptions} /> : <LocalOptions action={setCurrentOptions} />}
         </div>
       </IonContent>
     </IonPage>
