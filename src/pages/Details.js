40df58fbd8a58d68ce507371fdaa5eaef92cf150
rmenendezdaw <rmenendeziestacio@gmail.com>
2021-02-23 20:20:12 +0100

    Guests access to details and change buttons

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -45,10 +45,6 @@ const Details = () => {
   let players = Object.values(event.p);
   const comments = event.comments;
 
-  if (!state.user) {
-    return <Redirect to="/login" />;
-  }
-
   let goInstalation = (city) => {
     let pista = instalaciones.filter((pista) => pista.ubication == city);
 
@@ -94,7 +90,7 @@ const Details = () => {
         break;
     }
   }
-
+  if(state.user==''){
   return (
     <IonPage>
       <Header page={event.title + "#" + id}></Header>
@@ -172,39 +168,6 @@ const Details = () => {
                     item={event.author}
                   ></Author>
                 </div>
-                <IonButton
-                  style={{
-                    display:
-                      state.events_joined.indexOf(event.id) > -1
-                        ? "none"
-                        : "block",
-                  }}
-                  disabled={event.maxplayers == event.p.length ? true : false}
-                  className="event-card-content-left-join"
-                  color="success"
-                  onClick={() => {
-                    if (event.maxplayers == event.p.length) {
-                      setShowToast(true);
-                      setMessage("This event is completed");
-                    } else
-                      dispatch({ type: "SET_JOIN", value: event.id })}
-                  }
-                >
-                  JOIN
-                </IonButton>
-                <IonButton
-                  style={{
-                    display:
-                      state.events_joined.indexOf(event.id) > -1
-                        ? "block"
-                        : "none",
-                  }}
-                  className="event-card-content-left-join"
-                  color="success"
-                  onClick={() =>dispatch({ type: "REMOVE_JOIN", value: event.id })}
-                >
-                  REMOVE JOIN
-                </IonButton>
               </div>
 
               <div className="event-card-content-right">
@@ -280,6 +243,193 @@ const Details = () => {
       </IonContent>
     </IonPage>
   );
+}
+return (
+  <IonPage>
+    <Header page={event.title + "#" + id}></Header>
+    <IonContent fullscreen>
+      <IonHeader collapse="condense">
+        <IonToolbar>
+          <IonTitle size="large">{event.title}</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <div className="details-page">
+        <div className="event-card">
+          <div className="event-card-image">
+            <img src={event.image} alt="" />
+            <span
+              className="remove-event-btn"
+              onClick={() => deleteEvent()}
+              style={{
+                display:
+                  state.user.username === event.author.username
+                    ? "block"
+                    : "none",
+              }}
+            >
+              DELETE
+              <IonIcon icon={trash} />
+            </span>
+            <div className="event-card-image-badges">
+              <span
+                className="share-content badge-details badge-details-icon"
+                onClick={() => handleClick("was")}
+              >
+                <IonIcon icon={logoWhatsapp} />
+              </span>
+              {/* <span className="share-content badge-details badge-details-icon" onClick={()=>handleClick("disc")}>
+              <IonIcon icon={logoDiscord} />             
+              </span> */}
+              <span className="event-card-image-badges-time badge-details badge-details-blue">
+                <span className="badge-details-icon">
+                  <IonIcon icon={alarm} />
+                </span>
+                <span>{event.time}</span>
+              </span>
+              <span className="event-card-image-badges-location badge-details badge-details-green">
+                <span className="badge-details-icon">
+                  <IonIcon icon={compass} />
+                </span>
+                <span>{event.location.country}</span>
+              </span>
+              <span className="event-card-image-badges-location badge-details badge-details-green">
+                <span className="badge-details-icon">
+                  <IonIcon icon={compass} />
+                </span>
+                <span>{event.location.postalcode}</span>
+              </span>
+              <span className="event-card-image-badges-location badge-details badge-details-green">
+                <span className="badge-details-icon">
+                  <IonIcon icon={compass} />
+                </span>
+                <span>{event.location.city}</span>
+              </span>
+            </div>
+          </div>
+          <div className="event-card-content">
+            <div className="event-card-content-left">
+              <span className="event-card-content-left-title">
+                {event.title}
+              </span>
+              <span className="event-card-content-left-desc">
+                {event.description}
+              </span>
+              <div className="event-card-content-left-authorinfo">
+                <Author
+                  className="event"
+                  key={event.id}
+                  item={event.author}
+                ></Author>
+              </div>
+              <IonButton
+                style={{
+                  display:
+                    state.events_joined.indexOf(event.id) > -1
+                      ? "none"
+                      : "block",
+                }}
+                disabled={event.maxplayers == event.p.length ? true : false}
+                className="event-card-content-left-join"
+                color="success"
+                onClick={() => {
+                  if (event.maxplayers == event.p.length) {
+                    setShowToast(true);
+                    setMessage("This event is completed");
+                  } else
+                    dispatch({ type: "SET_JOIN", value: event.id })}
+                }
+              >
+                JOIN
+              </IonButton>
+              <IonButton
+                style={{
+                  display:
+                    state.events_joined.indexOf(event.id) > -1
+                      ? "block"
+                      : "none",
+                }}
+                className="event-card-content-left-join"
+                color="success"
+                onClick={() =>dispatch({ type: "REMOVE_JOIN", value: event.id })}
+              >
+                REMOVE JOIN
+              </IonButton>
+            </div>
+
+            <div className="event-card-content-right">
+              <span className="event-card-content-right-title">
+                PLAYERS {event.p.length + "/" + event.maxplayers}
+              </span>
+              <div className="event-card-content-right-players">
+                {players.map((player, index, arr) => (
+                  <div key={index} className="player">
+                    <img
+                      src="https://image.freepik.com/vector-gratis/perfil-avatar-hombre-icono-redondo_24640-14044.jpg"
+                      alt=""
+                    />
+                    <span>{player}</span>
+                  </div>
+                ))}
+              </div>
+              <span className="event-card-content-right-title">MAPA</span>
+              <p
+                className="mapsDetails"
+                onClick={() => {
+                  let newCoordinates = {
+                    lat: event.coordinates.lat,
+                    lng: event.coordinates.lng,
+                  };
+                  dispatch({
+                    type: "ALL_COORDINATES",
+                    value: newCoordinates,
+                  });
+                  setShowModal(true);
+                }}
+              ></p>
+              <span className="event-card-content-right-title">
+                INSTALATIONS
+              </span>
+              <div className="event-card-content-right-players">
+                <Link
+                  className="link"
+                  to={goInstalation(event.location.city)}
+                >
+                  <div className="cardContent__operation--icon ">
+                    <IonButton>
+                      <IonIcon
+                        icon={compassSharp}
+                        className="eventContent__actions--icon"
+                      />
+                      &nbsp;&nbsp; GO INSTALATIONS
+                    </IonButton>
+                  </div>
+                </Link>
+              </div>
+            </div>
+            <NFUComments comments={comments} gameID={event.id} />
+            {/* MODAL */}
+            <IonModal isOpen={showModal}>
+              <MyModal></MyModal>
+              <IonButton onClick={() => setShowModal(false)}>
+                Close Map
+              </IonButton>
+            </IonModal>
+
+            {/* END MODAL */}
+          </div>
+        </div>
+      </div>
+      <IonLoading isOpen={showLoading} message={"Deleting event"} />
+      <IonToast
+        isOpen={showToast}
+        onDidDismiss={() => setShowToast(false)}
+        message={message}
+        duration={2000}
+      />
+    </IonContent>
+  </IonPage>
+);
 };
+
 
 export default Details;
