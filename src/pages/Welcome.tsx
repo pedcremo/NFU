c0b299415b7797e1e81b18e58dfb9fdb8fc2a5a6
rmenendezdaw <rmenendeziestacio@gmail.com>
2021-02-23 21:48:07 +0100

    Next and preview slides(Medium-2)

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useContext, useState, useEffect, useRef } from 'react';
 import { AppContext } from '../State';
 import { Redirect } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
@@ -8,6 +8,7 @@ import {
   IonPage,
   IonSlide,
   IonSlides,
+  IonBackButton
 } from "@ionic/react";
 import "./Welcome.css";
 import icon from '../assets/img/nfu_icon.png'
@@ -15,6 +16,8 @@ import DeporteImg from "../assets/img/deporte_img.png";
 import geo from "../assets/img/geo.png";
 import friends from "../assets/img/friends-welcome.png";
 import ready from "../assets/img/ready.png";
+import { add } from "ionicons/icons";
+
 
 // import { Redirect } from 'react-router-dom';
 
@@ -23,6 +26,14 @@ const Welcome: React.FC = () => {
   const { state, dispatch } = useContext(AppContext);
   const [ welcome, setWelcome ] = useState<React.ReactText | undefined>('');
   const { t } = useTranslation();
+  const slideRef = useRef<HTMLIonSlidesElement>(null);
+  const next = () => slideRef.current.slideNext();
+  const preview = () => slideRef.current.slidePrev();
+
+  const slideOpts = {
+    initialSlide: 0,
+    speed: 400
+  };
 
   useEffect(() => {
     dispatch({ type: 'WELCOME', value: welcome})
@@ -34,8 +45,9 @@ const Welcome: React.FC = () => {
     <IonPage>
       <IonContent fullscreen className="welcome-page-content" scroll-y="false">
         <div className="welcome-page-content">
-          <IonSlides pager={true}>
-            <IonSlide>
+      
+          <IonSlides options={slideOpts} pager={true} ref={slideRef}>
+            <IonSlide >
               <div className="slide">
                 <div className="topSlide">
                   <div className="WelcomeAppTitleContainer">
@@ -48,7 +60,7 @@ const Welcome: React.FC = () => {
                     className="welcomeImg"
                   />
                 </div>
-
+                <button className="buttonSlide" onClick={next}>Next</button>
                 <h2 className="slideTitle">{t('welcome.welcome')}</h2>
                 <p className="slideText">
                 {t('welcome.description')} 
@@ -66,7 +78,8 @@ const Welcome: React.FC = () => {
                   </div>
                   <img src={friends} alt="Deporte IMG" className="welcomeImg" />
                 </div>
-
+                <button className="buttonSlide" onClick={preview}>Preview</button>
+                <button className="buttonSlide" onClick={next}>Next</button>
                 <h2 className="slideTitle">{t('welcome.meet')}</h2>
                 <p className="slideText">
                 {t('welcome.meetDescription')}
@@ -88,7 +101,8 @@ const Welcome: React.FC = () => {
                     className="welcomeImg"
                   />
                 </div>
-
+                <button className="buttonSlide" onClick={preview}>Preview</button>
+                <button className="buttonSlide" onClick={next}>Next</button>
                 <h2 className="slideTitle">{t('welcome.find')}</h2>
                 <p className="slideText">
                   {t('welcome.findDescription')}                  
@@ -109,12 +123,14 @@ const Welcome: React.FC = () => {
                     className="welcomeImg flipImg"
                   />
                 </div>
-
+                <button className="buttonSlide" onClick={preview}>Preview</button>
                 <h2 className="slideTitle">{t('welcome.ready')}</h2>
                 <IonButton fill="clear" className="welcome-btn continue-welcome-btn" onClick={() => setWelcome('true')}>{t('welcome.continue')}</IonButton>
               </div>
             </IonSlide>
+
           </IonSlides>
+
         </div>
       </IonContent>
     </IonPage>
