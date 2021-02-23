621a3046e846af0de0863caba488b7c15ca8013e
javisolercanto <jsolercanto11@gmail.com>
2021-02-23 20:41:08 +0100

    LARGE - 1

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,7 +1,7 @@
 import React, { useContext, useState} from "react";
 import { AppContext } from "../../State";
 import { Redirect, useHistory } from "react-router-dom";
-import { IonContent, IonPage, IonImg, IonLabel } from "@ionic/react";
+import { IonContent, IonPage, IonImg, IonLabel, IonAlert } from "@ionic/react";
 import "./login.css";
 import DeporteImg from "../../assets/img/deporte_img.png";
 import AppTitle from '../../components/shared/AppTitle'
@@ -15,6 +15,11 @@ const Login: React.FC = () => {
   const { t } = useTranslation();
   const history = useHistory();
   const HOME_PATH = "/app/home";
+
+  const CLICKS_TO_EASTER = 22;
+  const [currentClicks, setClick] = useState(0);
+  const [showRedeemCode, setShowRedeemCode] = useState(false);
+
   /* istanbul ignore if */
   if (state.welcome !== 'true') { return <Redirect to="/welcome" /> }
   if (state.user) return <Redirect to="/app/home" />;
@@ -22,9 +27,23 @@ const Login: React.FC = () => {
   return (
     <IonPage>
       <IonContent fullscreen>
+        <IonAlert
+          isOpen={showRedeemCode}
+          onDidDismiss={() => setShowRedeemCode(false)}
+          header={'Congratulations!'}
+          subHeader={'Here\'s your redeem code: '}
+          message={'20DISCOUNTCODE'}
+          buttons={['Thanks, I got it']}
+          onWillDismiss={() => setClick(0)}
+        />
         <div className="loginPageContent">
           <AppTitle />
-          <IonImg src={DeporteImg} alt="Deporte IMG" className="loginImg" />
+          <IonImg onClick={() => {
+            setClick(currentClicks + 1);
+            if (currentClicks === CLICKS_TO_EASTER) {
+              setShowRedeemCode(true);
+            }
+          }} src={DeporteImg} alt="Deporte IMG" className="loginImg" />
           <IonLabel className="prhaseLogin">{t("login.initial_text")}</IonLabel> 
           <IonLabel onClick={() => {
             history.push(HOME_PATH)
