8b073cdd712bce3374d5048e679a548fd0b4622f
j-maestre <xemaiestacio@gmail.com>
2021-02-23 20:02:50 +0100

    Faciles-> actividad2

--- README.md
+++ README.md
@@ -23,6 +23,3 @@ https://blog.bitsrc.io/testing-with-react-testing-library-and-jest-3dffaa114c04
 https://jestjs.io/docs/en/tutorial-react
 
 
-Actividades
-
-Faciles
--- src/pages/Details.js
+++ src/pages/Details.js
@@ -45,10 +45,6 @@ const Details = () => {
   let players = Object.values(event.p);
   const comments = event.comments;
 
-  if (!state.user) {
-    return <Redirect to="/login" />;
-  }
-
   let goInstalation = (city) => {
     let pista = instalaciones.filter((pista) => pista.ubication == city);
 
@@ -183,11 +179,16 @@ const Details = () => {
                   className="event-card-content-left-join"
                   color="success"
                   onClick={() => {
-                    if (event.maxplayers == event.p.length) {
-                      setShowToast(true);
-                      setMessage("This event is completed");
-                    } else
-                      dispatch({ type: "SET_JOIN", value: event.id })}
+                    if (!state.user) {
+                      history.push('/login')
+                    }else{
+                      if (event.maxplayers == event.p.length) {
+                        setShowToast(true);
+                        setMessage("This event is completed");
+                      } else
+                        dispatch({ type: "SET_JOIN", value: event.id })}
+                    }
+                    
                   }
                 >
                   JOIN
