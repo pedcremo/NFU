2dc58c894232727a512bd536655c866830f3ccc7
rmenendezdaw <rmenendeziestacio@gmail.com>
2021-02-23 23:40:49 +0100

    EasterEgg(Hard-1)

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,7 +1,7 @@
 import React, { useContext, useState} from "react";
 import { AppContext } from "../../State";
 import { Redirect, Link } from "react-router-dom";
-import { IonContent, IonPage, IonImg, IonLabel } from "@ionic/react";
+import { IonContent, IonPage, IonImg, IonLabel,  IonToast} from "@ionic/react";
 import "./login.css";
 import DeporteImg from "../../assets/img/deporte_img.png";
 import AppTitle from '../../components/shared/AppTitle'
@@ -9,10 +9,23 @@ import SocialOptions from './SocialOptions'
 import LocalOptions from './LocalOptions'
 import { useTranslation } from "react-i18next";
 
+
 const Login: React.FC = () => {
   const { state } = useContext(AppContext);
   const [ currentOptions, setCurrentOptions] = useState<React.ReactText | undefined>('Social');
   const { t } = useTranslation();
+  const [ easterEgg, setEasterEgg] = useState(0);
+  const [message, setMessage] = useState("");
+  const [showToast, setShowToast] = useState(false);
+  let easterEggClick = () => {
+    if(easterEgg>=22){
+      setMessage("YEEEEEEEEES, you find the EASTER EGG");
+      setShowToast(true);
+      setEasterEgg(0)
+    }else{
+      setEasterEgg(easterEgg+1)
+    }
+  }
 
   /* istanbul ignore if */
   if (state.welcome !== 'true') { return <Redirect to="/welcome" /> }
@@ -26,10 +39,16 @@ const Login: React.FC = () => {
             <IonLabel>{t("login.home")}</IonLabel>
         </Link>
           <AppTitle />
-          <IonImg src={DeporteImg} alt="Deporte IMG" className="loginImg" />
+          <IonImg src={DeporteImg} onClick={() => easterEggClick()} alt="Deporte IMG" className="loginImg" />
           <IonLabel className="prhaseLogin">{t("login.initial_text")}</IonLabel> 
           { currentOptions === "Social" ? <SocialOptions action={setCurrentOptions}/> : <LocalOptions action={setCurrentOptions}/> }
         </div>
+        <IonToast
+          isOpen={showToast}
+          onDidDismiss={() => setShowToast(false)}
+          message={message}
+          duration={2000}
+        />
       </IonContent>
     </IonPage>
   );
